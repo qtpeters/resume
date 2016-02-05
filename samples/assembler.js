71854ef44f4252849c3011ccfceab969b202e2ac
Quinn Peters <quinn.t.peters@gmail.com>
2016-02-05 05:57:33 -0500

    Splitting up JSON for simplicity of writing, developing tool to assemble the JSON

---
+++ samples/assembler.js
@@ -0,0 +1,25 @@
+#!/usr/local/bin/node
+
+module.exports = ( function() {
+   
+   var fs = require( 'fs' );
+   var Q = require( 'Q' );
+   var folder = process.argv[2];
+
+   if ( typeof folder === 'undefined' ) {
+      console.log( "Specify a folder" );
+   } else {
+      var mainFile = folder + '/' + folder + '.json';
+      var options = {
+         encoding: 'utf8'
+      }
+
+      fs.readFile( mainFile, options, function( err, data ) {
+         var main = JSON.parse( data );
+
+         
+         console.log( main ); 
+      });
+   }
+}());
+
